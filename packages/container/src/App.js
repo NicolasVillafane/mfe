caa7dc3ebc5b9593e10c3c996a0032632bd8c459
Nicolas <nivi1023@gmail.com>
2023-05-15 10:16:50 -0300

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import marketingApp from './components/marketingApp';
+import MarketingApp from './components/marketingApp';
+
+export default () => {
+  return (
+    <div>
+      <h1>Hi there!</h1> <hr />
+      <MarketingApp />
+    </div>
+  );
+};
